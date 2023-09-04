dc9a10d869f742e41d4e2fd3927e7a03598d2de9
João Paulo <joaowais@gmail.com>
2023-09-04 17:34:29 -0300

    Projeto Finalizado

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent {
+  @Input() gameCover: string = '';
 
+  @Input() gameLabel: string = '';
+
+  @Input() gameType: string = '';
+  @Input() gamePrice: string = '';
 }
